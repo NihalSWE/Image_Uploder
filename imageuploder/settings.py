47a6e27b30c048257bc223d1e768046a6d2f434a
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-05-01 17:58:44 +0600

    End of Project

--- imageuploder/settings.py
+++ imageuploder/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR/ "media"  # /root/projectname
+MEDIA_URL=  "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
